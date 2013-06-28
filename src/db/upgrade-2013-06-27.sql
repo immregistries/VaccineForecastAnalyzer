b13bfc229114b1568b5b207a883521be39844091
nathanbunker <nathanbunker@23f8a591-dcf1-47a8-acd2-7cc1bb7c4a6e>
2013-06-28 15:51:19 +0000

    Pulled out support for connecting to forecasters into separate project

--- src/db/upgrade-2013-06-27.sql
+++ src/db/upgrade-2013-06-27.sql
@@ -1,1 +1,16 @@
+
+update forecast_item set label = 'Hep A' where forecast_item_id = 4;
+update forecast_item set label = 'Hep B' where forecast_item_id = 5;
+update forecast_item set label = 'Tdap or Td' where forecast_item_id = 15;
+
 insert into forecast_item (forecast_item_id, label) values (16, 'PPSV');
+insert into forecast_item (forecast_item_id, label) values (17, 'PCV');
+insert into forecast_item (forecast_item_id, label) values (18, 'Td Only');
+insert into forecast_item (forecast_item_id, label) values (19, 'DTaP, Tdap or Td');
+insert into forecast_item (forecast_item_id, label) values (20, 'Hep B 2 Dose Only');
+insert into forecast_item (forecast_item_id, label) values (21, 'Hep B 3 Dose Only');
+insert into forecast_item (forecast_item_id, label) values (22, 'Measles Only');
+insert into forecast_item (forecast_item_id, label) values (23, 'Mumps Only');
+insert into forecast_item (forecast_item_id, label) values (24, 'Rubella Only');
+insert into forecast_item (forecast_item_id, label) values (25, 'Tdap Only');
+
