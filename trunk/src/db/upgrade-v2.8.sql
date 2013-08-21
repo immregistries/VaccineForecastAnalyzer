6c1c103d78620d93c221ec5b4d3672391d067f17
nathanbunker <nathanbunker@23f8a591-dcf1-47a8-acd2-7cc1bb7c4a6e>
2013-08-21 13:09:30 +0000

    Changes for v2.7 and v2.8 includes Login Attempt and ICE Integration

---
+++ trunk/src/db/upgrade-v2.8.sql
@@ -0,0 +1,5 @@
+INSERT INTO software (software_id, label, service_url, service_type) VALUES (12, 'ICE Forecaster', 'http://localhost:8086/o/evaluate', 'ice');
+
+INSERT INTO task_group (task_group_id, label, primary_software_id) VALUES (12, 'ICE User Group', 12);
+
+INSERT INTO expert (user_id, task_group_id, role_status) VALUES (2, 12, 'E'); -- Adding nathan to ICE User Group
--- trunk/src/main/java/org/tch/ft/SoftwareVersion.java
+++ trunk/src/main/java/org/tch/ft/SoftwareVersion.java
@@ -1,5 +1,5 @@
 package org.tch.ft;
 
 public class SoftwareVersion {
-  public static final String VERSION = "2.5";
+  public static final String VERSION = "2.8";
 }
