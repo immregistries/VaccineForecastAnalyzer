5998442c1c33a7df5d065d2de14c1f3c9265ce76
nathanbunker <nathanbunker@23f8a591-dcf1-47a8-acd2-7cc1bb7c4a6e>
2016-02-02 13:04:36 +0000

    Added MeningB to schedule

--- trunk/src/db/upgrade-v3.09.sql
+++ trunk/src/db/upgrade-v3.09.sql
@@ -1,5 +1,7 @@
 INSERT INTO vaccine_group (vaccine_group_id, label) VALUES (35, 'Mening Bexsero');
 INSERT INTO vaccine_group (vaccine_group_id, label) VALUES (36, 'Mening Trumenba');
+INSERT INTO vaccine_group (vaccine_group_id, label) VALUES (37, 'MeningB');
 
 UPDATE vaccine_group SET map_to_cdsi_code = 'MCV' WHERE label = 'Mening Bexsero';
 UPDATE vaccine_group SET map_to_cdsi_code = 'MCV' WHERE label = 'Mening Trumenba';
+UPDATE vaccine_group SET map_to_cdsi_code = 'MCV' WHERE label = 'MeningB';
